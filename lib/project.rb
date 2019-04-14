baa26a9a6f12abf8b23394a333853029e354d367
Michael Mottola <mottola64@gmail.com>
2019-04-14 22:55:17 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,8 +1,8 @@
 class Project
-attr_reader :backers
+attr_reader :backers, :title
 
-def initialize(backers)
-  @backers = backers
+def initialize(title)
+  @title = title
   @backers = []
 end  
   
