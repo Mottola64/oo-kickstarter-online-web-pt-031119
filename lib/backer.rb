c6321ac73091e8d67b9ffa3ac7e260c9582405bc
Michael Mottola <mottola64@gmail.com>
2019-04-14 22:55:55 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,8 +1,7 @@
 class Backer
-  attr_reader :backed_projects, :title
-  
-def initialize(title)
-  @title = title
+  attr_reader :backed_projects, :name
+def initialize(name)
+  @name = name
   @backed_projects = []
 end  
   
